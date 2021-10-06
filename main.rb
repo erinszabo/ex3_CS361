e9a6f9f8c266d0c98447b631937e7a6e99986f85
erinszabo <55733370+erinszabo@users.noreply.github.com>
2021-10-06 14:24:36 -0700

    fix demeter violation

--- main.rb
+++ main.rb
@@ -2,15 +2,13 @@ class Student
   
   MAX_COURSES = 5
   
-  def remove_from_schedule(course, schedule, quarter_id)
-    schedule.quarter[quarter_id].course_list.remove(course)
+  def remove_course(course, course_list)
+    course_list.remove(course)
   end
   
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
-    if max_courses < MAX_COURSES
+  def add_course(course, course_list)
+    course_limit = course_list.maximum_number_of_courses
+    if course_limit < MAX_COURSES
       course_list.add(course)
     end
   end
